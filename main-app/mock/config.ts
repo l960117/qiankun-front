1e35dc129fe6cfcf021f2d7bf2cdb60c3a35c6ef
lws <liaowensheng@ingbaobei.com>
2021-01-18 14:30:52 +0800

    feat: 去除多余路由

--- main-app/mock/config.ts
+++ main-app/mock/config.ts
@@ -17,86 +17,8 @@ export default {
         name: 'vue-sub-app-2',
         entry: '//localhost:8004'
       },
-      {
-        name: 'ims',
-        entry: '//localhost:8889'
-      },
-      {
-        name: 'live',
-        entry: '//tv.woniubaoxianyiyuan.com'
-      }
     ],
     routes: [
-      {
-        name: 'live',
-        path: '/live',
-        routes: [
-          {
-            name: 'liveList',
-            path: '/live/liveList'
-          },
-          {
-            name: 'customer',
-            path: '/live/customer/index'
-          }
-        ]
-      },
-      {
-        name: 'main1',
-        path: '/ims/index'
-      },
-      {
-        name: 'config',
-        path: '/ims/config',
-        routes: [
-          {
-            name: 'productBaseConfig',
-            path: '/ims/config/product-base-config'
-          },
-          {
-            name: 'productConfig',
-            path: '/ims/config/product-config'
-          },
-          {
-            name: 'mapping',
-            routes: [
-              {
-                name: 'professionManage',
-                path: '/ims/config/profession-manage'
-              },
-              {
-                name: 'areaManage',
-                path: '/ims/config/area-manage'
-              },
-              {
-                name: 'region',
-                path: '/ims/config/region'
-              }
-            ]
-          },
-          {
-            name: 'baseManage',
-            routes: [
-              {
-                name: 'baseArea',
-                path: '/ims/config/base-area'
-              },
-              {
-                name: 'baseBank',
-                path: '/ims/config/base-bank'
-              },
-              {
-                name: 'baseChannel',
-                path: '/ims/config/base-channel'
-              },
-              {
-                name: 'baseWidget',
-                path: '/ims/config/base-widget'
-              }
-            ]
-          }
-        ]
-      },
       {
         name: 'vue-sub-app-1',
         path: '/vue-sub-app-1',
